4279eedaa21a9ace66d371507cb7c5b404fb6907
Gaël Gamba <gamba.gael@orange.fr>
2022-01-04 08:27:50 +0100

    Added voice recognition

--- backend/ActionManager.py
+++ backend/ActionManager.py
@@ -1,4 +1,5 @@
 import socket
 
 class ActionManager:
+
     pass
--- backend/ChatManager.py
+++ backend/ChatManager.py
@@ -1,4 +1,17 @@
 import socket
+import speech_recognition as sr  
 
-class ActionManager:
-    pass
+
+
+# get audio from the microphone                                                                       
+r = sr.Recognizer()                                                                                   
+with sr.Microphone() as source:                                                                       
+    print("Speak:")                                                                                   
+    audio = r.listen(source)   
+
+try:
+    print("You said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
