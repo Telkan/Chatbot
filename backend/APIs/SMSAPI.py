3a52666621ae2ad513c52fb6e23dcc8cab92f220
Gaël Gamba <gamba.gael@orange.fr>
2022-01-05 14:08:33 +0100

    Unfinished connection of everything

---
+++ backend/APIs/SMSAPI.py
@@ -0,0 +1,10 @@
+
+
+class SMSAPI:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    
--- backend/ActionManager.py
+++ backend/ActionManager.py
@@ -1,5 +1,29 @@
 import socket
+from backend.APIs import SMSAPI
 
 class ActionManager:
+    def __init__(self):
+        #define apis to be listened to
+        self.sms = SMSAPI()
+
+        # start threads for every api
+        pass
+        
+    def handle_incoming_data(self, msgType, msg):
+        
+        switch msgType:
+            case 'SMS':
+                break
+
+        pass
 
     pass
+
+
+# just for testing
+class chatbot_action_object:
+    def __init__(self):
+        pass
+
+    def handleSMS(self, text):
+        print("sending sms '" + text + "' to chatbot...")
