e94488186f56aabcc01c3071204e9f34a4c7de17
MargotCobourg <margotcobourg@gmail.com>
2022-01-06 17:03:34 +0100

    Adding of Discord API V1

--- API/rasa_connection.py
+++ API/rasa_connection.py
@@ -1,5 +1,5 @@
 def curl_request(message, user):
-	import requests 
+	import requests
 	import json
 
 	headers = {
@@ -8,8 +8,10 @@ def curl_request(message, user):
 
 	#Later change to every user
 	data = '{"sender": "'+ user +'","message": "'+ message +'","metadata": {}}'
+	print(data)
 	response = requests.post('http://localhost:5005/webhooks/myio/webhook', headers=headers, data=data)
 
 	messages = json.loads(response.content)
+	print(messages)
 	answer = list(map(lambda msg: msg['image'] if 'image' in msg else msg['text'], messages))
 	return answer
