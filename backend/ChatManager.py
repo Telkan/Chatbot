0fd93fcfe55448e37d7a0c5560cf8d91d6904f53
Telkan <gamba.gael@orange.fr>
2022-01-06 10:13:37 +0100

    IT JUST WORKS C'EST LA TEUFEU

--- backend/ChatManager.py
+++ backend/ChatManager.py
@@ -1,10 +1,15 @@
 import speech_recognition as sr  
 from gtts import gTTS
 from playsound import playsound
+import requests
 class ChatManager:
 
+    def __init__(self) -> None:
+        self.api_url = "http://localhost:5005/webhooks/rest/webhook"
+        pass
+    
 
-    def textToSpeech(outputText):
+    def textToSpeech(self,outputText):
         tts = gTTS(outputText)
         tts.save('temp.mp3')
 
@@ -17,19 +22,37 @@ class ChatManager:
                 tts = gTTS(outputText)
                 tts.save('temp.mp3')
 
-    def listenForVoice():
+    def listenForVoice(self):
         r = sr.Recognizer()                                                                                   
         with sr.Microphone() as source:                                                                       
             print("Speak:")                                                                                   
-            audio = r.listen(source,10,7)       
+            audio = r.listen(source,10,5)       
         try:
             print("You said " + r.recognize_google(audio))
             tts = gTTS(r.recognize_google(audio))
             return r.recognize_google(audio)
         except sr.UnknownValueError:
             print("Could not understand audio")
-            return "Could not understand"
+            return "ERR-BadAudio"
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))    
-            return "Could not request results"                                                            
-    
+            return "ERR-DontWork"                              
+
+    def sendToChatbot(self, textToSend):                
+        self.chatMessage = {"sender": "Gael", "message": textToSend}
+        response = requests.post(self.api_url, json=self.chatMessage)
+        print(response.json())
+        return response.json()
+
+    def startComProgram(self):
+        while True:
+            message = self.listenForVoice()
+            if "ERR" in message:
+                continue  
+            answer = self.sendToChatbot(message)
+            print(type(answer[0]["text"]))
+            self.textToSpeech(answer[0]["text"])
+
+                              
+chat = ChatManager()
+chat.startComProgram()
