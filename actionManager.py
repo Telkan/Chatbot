041b1b747fdd469da6ba5dc115823005a0e013fd
Lucas <lucassmontagnee@gmail.com>
2022-01-13 19:33:12 +0100

    Action manager can handle the events
    There are 4 more functions & 3 more ActionManagerObject's methods

--- actionManager.py
+++ actionManager.py
@@ -1,8 +1,10 @@
 import json
 import threading
 import socket
+import time
 from datetime import datetime
 from API.send_messages import send_msg
+
 # Pour la V0 -- simulation
 # Trouver un message d'un certain medium/contact
 # Trouver un message d'un certain medium/contact a une date donnée
@@ -10,9 +12,9 @@ from API.send_messages import send_msg
 # Pour la V1 -- API
 # Informer d'une nouvelle arrivée de message
 
-#SMS_DB = "Databases/SMS/SMS_DataBase.json"
-#VM_DB = "Databases/Voicemails/Voicemail_DataBase.json"
-#CALLS_DB = "Databases/Calls/Calls_DataBase.json"
+# SMS_DB = "Databases/SMS/SMS_DataBase.json"
+# VM_DB = "Databases/Voicemails/Voicemail_DataBase.json"
+# CALLS_DB = "Databases/Calls/Calls_DataBase.json"
 GENERAL_DB = "Databases/General_DataBase_test.json"
 GENERAL_EVENT_DB = "Databases/General_event_DataBase_test.json"
 
@@ -24,6 +26,7 @@ DISCORD_medium = "DISCORD"
 ACTION_MANAGER_SERVER_PORT = 44444
 
 DATABASE_LOCK = threading.Lock()
+
 
 def dateInInterval(date1, date2):
     # date format : "05/01/2022, 15:01:34"
@@ -51,6 +54,7 @@ def sameDates(date1, date2):
     return day1 == day2 and month1 == month2 and year1 == year2
 
 
+# Date 1 after date 2 ?
 def isAfter(date1, date2):
     day1 = int(date1[0:2])
     month1 = int(date1[3:5])
@@ -93,11 +97,11 @@ def find_message(medium, contact, date='', datetype=1, usertype="all", last=0):
     # Datetype = 1 ==> All day
     # Usertype = "contact", "user", "all"
     # Last = 1 searching last message
-    if date == '':   # if we want to create the date of the new msg to add
+    if date == '':  # if we want to create the date of the new msg to add
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y, %H:%M:%S")
         date = dt_string
-    
+
     if last:
         return searchLast(medium, contact)
     if medium == SMS_medium:
@@ -171,48 +175,51 @@ def find_message(medium, contact, date='', datetype=1, usertype="all", last=0):
             rk += 1
         myfile.close()
     return "No message found"
-        
+
 
 """ DONT USE this func outside of this file! look at bottom actionManagerObject below instead """
-def add_message(medium='', date='', sender='', text='', fromUser=False, phone='null'):
 
-    if medium == '' or sender == '' or text == '': return False # fail check, abort call
-    
-    if date == '':   # if we want to create the date of the new msg to add
+
+def add_message(medium='', date='', sender='', text='', fromUser=False, phone='null'):
+    if medium == '' or sender == '' or text == '': return False  # fail check, abort call
+
+    if date == '':  # if we want to create the date of the new msg to add
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y, %H:%M:%S")
         date = dt_string
-    
-    if medium == VM_medium or medium == CALLS_medium:
-        if medium == VM_medium: text = 'Databases/Voicemails/' + text
 
-        else: text = 'Databases/Calls/' + text
+    if medium == VM_medium or medium == CALLS_medium:
+        if medium == VM_medium:
+            text = 'Databases/Voicemails/' + text
+
+        else:
+            text = 'Databases/Calls/' + text
 
     with open(GENERAL_DB, 'r+') as file:
         file_data = json.load(file)
 
-        active_db = medium + '_History'     # if its an sms, we work in the sms_db index of the DB
+        active_db = medium + '_History'  # if its an sms, we work in the sms_db index of the DB
 
         size = len(file_data[active_db])
 
         if fromUser:
-            new_msg = { 'date':date, 'from':'user', 'text':text}
+            new_msg = {'date': date, 'from': 'user', 'text': text}
         else:
-            new_msg = { 'date':date, 'from':sender, 'text':text }
+            new_msg = {'date': date, 'from': sender, 'text': text}
 
         for i in range(size):
             if file_data[active_db][i]["contactname"] == sender:
                 file_data[active_db][i]["messages"].append(new_msg)
                 break
-            if i + 1 == size: #contact doesnt exist in db
+            if i + 1 == size:  # contact doesnt exist in db
                 new_contact = {
-                        "contactname": sender,
-                        "mobilenumber": phone,
-                        "messages": [new_msg]}
-                
+                    "contactname": sender,
+                    "mobilenumber": phone,
+                    "messages": [new_msg]}
+
                 file_data[active_db].append(new_contact)
                 break
-        
+
         # Sets file's current position at offset.
         file.seek(0)
         # convert back to json.
@@ -220,17 +227,66 @@ def add_message(medium='', date='', sender='', text='', fromUser=False, phone='n
         return True
 
 
-class  ActionManagerObject:
+def write_json(new_data, filename='SMS_DataBase.json'):
+    with open(filename, 'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["events"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
+        file.close()
+
+
+def add_event(name, date):
+    mydata = {
+        "name_event": name,
+        "date": date
+    }
+    write_json(mydata, filename=GENERAL_EVENT_DB)
+
+
+def remove_event(name, date):
+    with open(GENERAL_EVENT_DB, 'r+') as file:
+        data = json.load(file)
+        rk = 0
+        for evt in data['events']:
+            if evt['name_event'] == name and evt['date'] == date:
+                del data['events'][rk]
+                break
+            rk += 1
+        file.close()
+    with open(GENERAL_EVENT_DB, 'w') as data_file:
+        json.dump(data, data_file, indent=4)
+        data_file.close()
+
+
+def find_event():
+    myEvents = []
+    date = datetime.now()
+    dt_string = date.strftime("%d/%m/%Y, %H:%M:%S")
+    date = dt_string
+    with open(GENERAL_EVENT_DB, 'r+') as file:
+        data = json.load(file)
+        for evt in data['events']:
+            if isAfter(evt['date'], date):
+                myEvents.append(evt)
+        return myEvents
+
+
+class ActionManagerObject:
+
     def __init__(self, chat_manager):
         self.cm = chat_manager
-        pass        
+        pass
 
     def lookup_message(self, medium, contact, date='', datetype=1, usertype="all", last=0):
         # here we have the lookup function
+        return find_message(medium.upper(), contact, date, datetype, usertype, last)
 
-        return find_message(medium.upper(), contact, date, datetype, usertype, last) 
-    
-    def add_message(self, medium, date='', sender, text, phone='null', fromUser=False):
+    def add_message(self, medium, sender, text, date='', phone='null', fromUser=False):
         # here we have add message logic, db-related etc.
         medium = medium.upper()
 
@@ -239,32 +295,32 @@ class  ActionManagerObject:
             print('AM: Failed to add new message to db. Abort Chatmanager call.')
             return
 
-        if not fromUser: # code for contacting CM
+        if not fromUser:  # code for contacting CM
             self.cm.handle_action_manager_msg(medium, sender)
 
-    def lookup_event(self, args):
-        # lookup event db
-        pass
+    def lookup_event(self):
+        return find_event()
 
-    def add_event(self, args):
-        # add event to db
-        pass
+    def add_event(self, name, date):
+        add_event(name, date)
+
+    def rm_event(self, name, date):
+        remove_event(name, date)
 
     def send_message(self, medium, contact, text):
         medium = medium.upper()
 
-        self.add_message(medium, '', contact, text,'18',fromUser=True)
+        self.add_message(medium, '', contact, text, '18', fromUser=True)
 
         if DISCORD_medium in medium:
-            #send_msg(text)
+            # send_msg(text)
             try:
                 send_msg(text)
             except:
                 print('AM: sending discord msg failed.')
-            break
-            
-        #elif SMS_medium in medium:
-            
+
+        # elif SMS_medium in medium:
+
         elif VM_medium in medium:
             pass
         elif CALLS_medium in medium:
@@ -273,15 +329,13 @@ class  ActionManagerObject:
             print('AM: unknown medium, trying to send message: ', medium)
 
 
-
-#""" FUNCTION WHICH WILL RECIEVE MESSAGES """
+# """ FUNCTION WHICH WILL RECIEVE MESSAGES """
 def api_message_listener(AM):
-
     print('AM_server:--==AM server LIVE==--')
 
-    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM )
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-    sock.bind(('127.0.0.1', ACTION_MANAGER_SERVER_PORT))    # setup server
+    sock.bind(('127.0.0.1', ACTION_MANAGER_SERVER_PORT))  # setup server
 
     while True:
         # PROTOCOL for sending API messages to this listener         
@@ -290,16 +344,20 @@ def api_message_listener(AM):
         msg = msg_bytes.decode("utf-8")
         els = msg.split('*')
 
-        if len(els)!=5: continue
+        if len(els) != 5: continue
         print('in AM server:', els)
         with DATABASE_LOCK:
-            AM.add_message(els[0], '' if els[1]=='null' else els[1], els[2], els[4])
+            AM.add_message(els[0], '' if els[1] == 'null' else els[1], els[2], els[4])
         return
 
+
 if __name__ == "__main__":
-    #add_message(medium=SMS_medium, sender='Alex', text='testing', phone='074392345',fromUser=True)
-    #add_message(medium=DISCORD_medium, sender='Alex', text='testing2', phone='null', fromUser=True)
-    #aa = None
-    #AM = ActionManagerObject(aa)
-    #msgs = AM.lookup_message('Discord', 'swebent',last=1)
+    # add_message(medium=SMS_medium, sender='Alex', text='testing', phone='074392345',fromUser=True)
+    # add_message(medium=DISCORD_medium, sender='Alex', text='testing2', phone='null', fromUser=True)
+    # aa = None
+    # AM = ActionManagerObject(aa)
+    # msgs = AM.lookup_message('Discord', 'swebent',last=1)
+    # add_event("tofind", "15/01/2022, 20:00:00")
+    # print(find_event())
+    # remove_event("tofind", "15/01/2022, 20:00:00")
     pass
